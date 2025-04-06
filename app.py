abbaa4da7777b72f5a94f65d511e0276f2d663cf
priestking63@gmail.com <you@example.com>
2025-04-06 13:45:58 +0300

    3\

--- app.py
+++ app.py
@@ -9,3 +9,25 @@ def say_hello():
 @app.get("/sum")
 def sum_two(a: int, b: int) -> int:
     return a+b
+
+@app.get("/number/{number}")
+def print_num(number:int):
+    return number * 2
+
+
+@app.post('/user')
+def print(user: str):
+    return {'message': f'hello, {user}'}
+
+@app.get('/booking/all')
+def all_bookings():
+    conn = psycopg2.connect(
+        "postgresql://postgres:password@localhost:5432/exercises"
+        )
+    cursor = conn.cursor()
+    cursor.execude(
+        """
+        SELECT *
+        FROM cd.bookings
+        """)
+    return cursor.fetchall()
