344543fdf0c98cdb92582601230066397142e13a
priestking63@gmail.com <you@example.com>
2025-04-06 16:22:05 +0300

    6y6

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
-@app.get('/user/{id}')
-def user_id(id:int):
-   conn = psycopg2.connect(
+class PostResponse(BaseModel):
+    id : int
+    text: str
+    topic: str
+
+    class Config:
+        orm_mode = True
+
+def get_db():
+    conn = psycopg2.connect(
     "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml",
     cursor_factory=RealDictCursor
    )
-   cursor = conn.cursor()
-   cursor.execute('SELECT gender, age, city FROM "user" WHERE id = %s;', (id,))
-   result = cursor.fetchone()
-   if result == []:
-        raise HTTPException(404,{"detail": "user not found"})
-    else:
-        return result
+    return conn
+
+@app.get('/post/{id}',response_model=PostResponse)
+def post_id(id:int, db = Depends(get_db))->PostResponse:
+    with db.cursor() as cursor:
+        cursor.execute(
+             f"""SELECT id, text, topic
+                 FROM post
+                 WHERE id = {id}
+              """)
+        result = cursor.fetchone()
+        if not result:
+            raise HTTPException(404)
+        else:
+            return PostResponse(**result)
+    
